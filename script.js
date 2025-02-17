37407fda60f813baaa63ba5392dac2e50088dcd4
ShatVM <pzs_shvm@ztu.edu.ua>
2025-02-17 11:20:02 +0200

    Add JavaScript functionality for wishes button and minor HTML adjustments

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Script connected');
+
+document.getElementById('btn_wishes').addEventListener('click', () => {
+    //alert('Button clicked');
+    document.getElementById ('p_wishes').innerText = 'Кохайте один одного! Це найважливіше!';  
+})
